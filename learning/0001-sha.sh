8f5371cc0c481798864a8ba19ea5f464933ea88c
Felipe Campos Penha <fcpenha@Mac.attlocal.net>
2025-10-26 20:42:04 -0700

    refactor.

--- learning/0001-sha.sh
+++ learning/0001-sha.sh
@@ -1,15 +1,13 @@
-
-
 # The shasum command is used to compute and check message digests.
-# The -a option, for algorithm, specifies the digest algorithm to use.
+# The --algorithm option specifies the digest algorithm to use.
 # In this case, 256 stands for SHA-256.
 
 echo "My secret data is here." > /tmp/mock.txt
 
-shasum -a 256 /tmp/mock.txt
+shasum --algorithm 256 /tmp/mock.txt
 
 echo "Addition." >> /tmp/mock.txt
 
-shasum -a 256 /tmp/mock.txt
+shasum --algorithm 256 /tmp/mock.txt
 
 rm /tmp/mock.txt
